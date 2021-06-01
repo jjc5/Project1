6fbbff293737f35418dd6ca0b477138b70788a4d
Jason C <jayingmantis.555@gmail.com>
2021-06-01 00:03:04 -0500

    game class made w/ methods. Functions for adding score. p1/p2 added

--- js/script.js
+++ js/script.js
@@ -13,15 +13,36 @@ const cell6 = document.querySelector('body > div.cells > button.cell6');
 const cell7 = document.querySelector('body > div.cells > button.cell7');
 const cell8 = document.querySelector('body > div.cells > button.cell8');
 const cell9 = document.querySelector('body > div.cells > button.cell9');
-let allCells = document.querySelectorAll('body > div.cells > button.cellBtn');
+// let allCells = document.querySelectorAll('body > div.cells > button.cellBtn');
 let seconds = document.getElementById("countdown").textContent;
 let cellArray = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9];
 let randomCell = cellArray[Math.floor(Math.random() * cellArray.length)];
-console.log(allCells)
-const style = getComputedStyle(cell1);
-const cell1Background = style.backgroundColor;
-const player1Score = [];
-const player2Score = [];
+
+class React {
+  constructor(name, score, highscore){
+    this.name = name;
+    this.score = score;
+    this.highscore = highscore;
+  }
+  addScore(){
+    this.score++;
+    this.updateStatsOnDom();
+  }
+  updateStatsOnDom(){
+    const container = document.querySelector('.scores');
+    container.innerHTML = `
+    <div class='player1-score'>
+      <span>${this.score}</span>
+    </div>
+    <div class='player2-score'>
+      <span>${this.score}</span>
+    </div>
+    `
+  }
+}
+const player1 = new React('player1', 0, 0);
+const player2 = new React('player2', 0, 0);
+console.log(player1.score)
 
 
 
@@ -34,13 +55,38 @@ const toggleClass = (node, className) => {
   node.classList.toggle(className)
 }
 /*^^^toggles the modal class 'open'*/
+const addClicksP1 = () => player1.addScore();
+const addClicksP2 = () => player2.addScore();
 
-function randomCellBg(e){
-    randomCell.style.backgroundColor = 'rgb(144, 200, 252)';
+const randomCellOg = function randomCellOg(){
+  let cellArray = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9];
+  randomCell = cellArray[Math.floor(Math.random() * cellArray.length)];
+  randomCell.style.backgroundColor = 'rgb(255, 182, 193)';
+  randomCell.addEventListener('click', (e) =>{
+    randomCellOg();
+  });
 }
 
+const randomCellBg = function randomCellBg() {
+    let cellArray = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9];
+    if (randomCell != undefined){ randomCell.removeEventListener('click', randomCellBg) }
+    randomCell = cellArray[Math.floor(Math.random() * cellArray.length)];
+    randomCell.style.backgroundColor = 'rgb(144, 200, 252)';
+    randomCell.addEventListener('click', (e) =>{
+      randomCellBg();
+      randomCellOg();
+    });
+}
 /*^^^chnages background of random cell*/
-
+const countClick = function countClick(){
+  let cellArray = [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9];
+  let randomCell = cellArray[Math.floor(Math.random() * cellArray.length)];
+  document.querySelector('body > div.cells').addEventListener('click', function addCounter(event) {
+    let element = event.currentTarget;
+    element.clicks = (element.clicks || 0) + 1;
+    console.log(element.clicks)
+  });
+}
 
 let countdown = function countdown(e){
   e.preventDefault();
@@ -59,24 +105,59 @@ let countdown = function countdown(e){
 playBtn.addEventListener('click', (e) =>{
   document.querySelector('body > div.controls > button.stop-button').style.visibility = 'visible';
   document.querySelector('body > div.controls > button.start-button').style.visibility = 'visible';
-  document.querySelector('body > h1.player1-score').style.visibility = 'visible';
-  document.querySelector('body > h1.player2-score').style.visibility = 'visible';
+  document.querySelector('body > div.scores > div.player1-score').style.visibility = 'visible';
+  document.querySelector('body > div.scores > div.player2-score').style.visibility = 'visible';
   document.querySelector('body > div.cells').style.visibility = 'visible';
   document.querySelector('body > div#countdown').style.visibility = 'visible';
   toggleClass(modal, 'open')
 });
-startBtn.addEventListener('click', (e)=>{
-  countdown(e);
-  randomCellBg(e);
-});
+startBtn.addEventListener('click', (e) =>{
+  // countdown(e);
 
-cell1.addEventListener('click', (e) =>{
-  if(cell1Background === 'rgb(255, 182, 193)'){
-     randomCellBg(e);
-  }
-});
+  toggleClass(randomCell, 'openBtn');
 
-console.log(cell1Background)
-console.log('hello')
-/*call functions*/
-// resetCountdown();
+  randomCellBg();
+});
+// randomCell.addEventListener('click', (e)=>{
+//   toggleClass(randomCell, 'openBtn');
+//
+// });
+ cell1.addEventListener('click', (e) =>{
+   toggleClass(randomCell, 'openBtn');
+   randomCellBg();
+ });
+ cell2.addEventListener('click', (e) =>{
+   toggleClass(randomCell, 'openBtn');
+   randomCellBg();
+ });
+ cell3.addEventListener('click', (e) =>{
+   toggleClass(randomCell, 'openBtn');
+   randomCellBg();
+ });
+ cell4.addEventListener('click', (e) =>{
+   toggleClass(randomCell, 'openBtn');
+   randomCellBg();
+ });
+ cell5.addEventListener('click', (e) =>{
+   toggleClass(randomCell, 'openBtn');
+   randomCellBg();
+ });
+ cell6.addEventListener('click', (e) =>{
+   toggleClass(randomCell, 'openBtn');
+   randomCellBg();
+ });
+ cell7.addEventListener('click', (e) =>{
+   toggleClass(randomCell, 'openBtn');
+   randomCellBg();
+ });
+ cell8.addEventListener('click', (e) =>{
+   toggleClass(randomCell, 'openBtn');
+   randomCellBg();
+ });
+ cell9.addEventListener('click', (e) =>{
+   toggleClass(randomCell, 'openBtn');
+   randomCellBg();
+ });
+
+
+countClick();
